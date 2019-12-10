3a38bf4d99f4d2e8e16074f739d249a4a18674f6
AmyMorand <amy.morand1999@gmail.com>
2019-12-09 23:51:14 -0500

    ddhhd

---
+++ JS/inner3.js
@@ -0,0 +1,26 @@
+function check(form)
+{
+
+if(form.pwd.value == "fep4Uju")
+{
+	return true;
+}
+else
+{
+	alert("Error Password or Username")
+	return false;
+}
+}
+
+
+$("#con").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        submitMSG(false, "Did you fill in the form properly?");
+        // handle the invalid form...
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
